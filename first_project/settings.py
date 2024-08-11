1976809dc09ab46502222fc3f30d6bc1457af323
Drew Goldstein <goldsteindrew314@gmail.com>
2024-08-11 13:32:45 -0400

    Added requirements.txt

--- first_project/settings.py
+++ first_project/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'first_app'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'first_app',
 ]
 
 MIDDLEWARE = [
